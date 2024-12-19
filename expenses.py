db1a8a72380c9d9fdc0255302f7b4aba0fd57eed
Engla Janze <111757818+Englajanze@users.noreply.github.com>
2024-12-19 11:00:46 +0100

    .

--- expenses.py
+++ expenses.py
@@ -3,4 +3,19 @@
 # take input of catagory, date and amount
 # ensure that it follows validate inputs
 
-user_add_expenses = input("Add your expenses here:");
+import json
+
+EXPENSES_FILE = "data/expenses.json"
+
+def load_expenses():
+    # Loading expenses from JSON file
+    try:
+        with open(EXPENSES_FILE, "r") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        # If the file doesn't exist or is empty, return an empty list
+        return []
+
+
+expenses = load_expenses()
+print("Current Expenses:", expenses)
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 # main page, connect all files here
 # should contain a meny where user can choose to display
 # add expenses, view expenses, view budget, view spending trends, view savings, set or view goals
+import streamlit as st
+
+st.title("My Budget Tracking App")
+st.write("Hello, world hello!")
